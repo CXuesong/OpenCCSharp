f193f283157c5c7e20806b7f965163470d2f77c0
forest93 <forest93@163.com>
2021-10-30 23:06:44 +0800

    Dict / Lexer API.

---
+++ OpenCCSharp.Conversion/IReadOnlyStringPrefixDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCCSharp.Conversion;
+
+public interface IReadOnlyStringPrefixDictionary<TValue> : IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>
+{
+
+    TValue this[ReadOnlySpan<char> key] { get; }
+
+    TValue IReadOnlyDictionary<ReadOnlyMemory<char>, TValue>.this[ReadOnlyMemory<char> key] => this[key.Span];
+
+    bool TryGetValue(ReadOnlySpan<char> key, [MaybeNullWhen(false)] out TValue value);
+
+    bool TryGetLongestPrefixingKey(ReadOnlySpan<char> content, out ReadOnlyMemory<char> key);
+
+    IEnumerable<ReadOnlyMemory<char>> EnumPrefixingKeys(ReadOnlySpan<char> content);
+
+    /// <inheritdoc />
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
+
---
+++ OpenCCSharp.Conversion/IScriptLexer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCCSharp.Conversion;
+
+public interface IScriptLexer
+{
+
+    IEnumerable<(int Start, int Length)> EnumSegments(ReadOnlySpan<char> content);
+
+}
