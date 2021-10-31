195a388371bfb185fcb5913a2269fd4fd033c6e6
forest93 <forest93@163.com>
2021-10-31 17:08:12 +0800

    Add ScriptConverter.Convert(string) => string.

--- OpenCCSharp.Conversion/ScriptConverter.cs
+++ OpenCCSharp.Conversion/ScriptConverter.cs
@@ -1,4 +1,7 @@
-﻿namespace OpenCCSharp.Conversion;
+﻿using System.Buffers;
+using System.Diagnostics;
+
+namespace OpenCCSharp.Conversion;
 
 public class ScriptConverter
 {
@@ -44,7 +47,38 @@ public class ScriptConverter
                 destinationConsumed += len;
             }
         }
+        Debug.Assert(srcRest.IsEmpty);
         completed = true;
+    }
+
+    public string Convert(string source) => Convert(source.AsSpan());
+
+    public string Convert(ReadOnlySpan<char> source)
+    {
+        if (source.IsEmpty) return "";
+        var rest = source;
+        var buffers = new List<(char[] Buffer, int Length)>();
+        var destLength = 0;
+        do
+        {
+            // Assumes the converted content has the same length of input.
+            var buffer = ArrayPool<char>.Shared.Rent(rest.Length);
+            Convert(rest, buffer.AsSpan(), out var sourceConsumed, out var destConsumed, out var completed);
+            Debug.Assert(sourceConsumed > 0);
+            buffers.Add((buffer, destConsumed));
+            destLength += destConsumed;
+            rest = source[sourceConsumed..];
+            Debug.Assert(!(completed && !rest.IsEmpty));
+        } while (!rest.IsEmpty);
+        if (destLength == 0) return "";
+        return string.Create(destLength, buffers, (sp, bufs) =>
+        {
+            foreach (var (buf, len) in bufs)
+            {
+                buf.AsSpan(0, len).CopyTo(sp);
+                sp = sp[len..];
+            }
+        });
     }
 
 }
