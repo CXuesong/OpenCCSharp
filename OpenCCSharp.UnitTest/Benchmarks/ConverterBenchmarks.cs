d92a2e12b7ae63880dd116e7dceb665468907fee
forest93 <forest93@163.com>
2022-01-23 23:26:21 +0800

    Use file-scope namespace.

--- OpenCCSharp.UnitTest/Benchmarks/ConverterBenchmarks.cs
+++ OpenCCSharp.UnitTest/Benchmarks/ConverterBenchmarks.cs
@@ -9,59 +9,58 @@ using OpenCCSharp.Presets;
 using OpenCCSharp.UnitTest.TestCases;
 using Xunit;
 
-namespace OpenCCSharp.UnitTest.Benchmarks
+namespace OpenCCSharp.UnitTest.Benchmarks;
+
+public class ConverterBenchmarks
 {
-    public class ConverterBenchmarks
+
+    [Benchmark]
+    [ArgumentsSource(nameof(EnumOpenCCTestArguments))]
+    public void OpenCCTest(OpenCCTestArguments arguments)
     {
-
-        [Benchmark]
-        [ArgumentsSource(nameof(EnumOpenCCTestArguments))]
-        public void OpenCCTest(OpenCCTestArguments arguments)
+        foreach (var input in arguments.TestCases)
         {
-            foreach (var input in arguments.TestCases)
-            {
-                arguments.Converter.Convert(input);
-            }
+            arguments.Converter.Convert(input);
         }
-
-        public IEnumerable<OpenCCTestArguments> EnumOpenCCTestArguments()
-        {
-            var method = typeof(ConverterTests).GetMethod(nameof(ConverterTests.OpenCCTest));
-            var argsAttributes = method!.GetCustomAttributes(typeof(InlineDataAttribute), true).Cast<InlineDataAttribute>();
-            var cases = argsAttributes.SelectMany(a => a.GetData(method))
-                .Select(a => new OpenCCTestArguments((ChineseConversionVariant)a[0], (ChineseConversionVariant)a[1], (string)a[2]))
-                .ToList();
-            return cases;
-        }
-
-        // private IEnumerable<OpenCCTestArguments>
-
-        public class OpenCCTestArguments
-        {
-            public ChineseConversionVariant FromVariant { get; }
-
-            public ChineseConversionVariant ToVariant { get; }
-
-            public string CaseSetName { get; }
-
-            public ScriptConverterBase Converter { get; }
-
-            public List<string> TestCases { get; }
-
-            public OpenCCTestArguments(ChineseConversionVariant fromVariant, ChineseConversionVariant toVariant, string caseSetName)
-            {
-                FromVariant = fromVariant;
-                ToVariant = toVariant;
-                CaseSetName = caseSetName;
-                // FIXME sync wait on async values.
-                Converter = ChineseConversionPresets.GetConverterAsync(fromVariant, toVariant).AsTask().GetAwaiter().GetResult();
-                // We only keep input for sake of profiling.
-                TestCases = OpenCCUtils.ReadTestCases(caseSetName).Select(t => t.Input).ToList();
-            }
-
-            /// <inheritdoc />
-            public override string ToString() => $"{FromVariant} -> {ToVariant} [{CaseSetName}]";
-        }
-
     }
+
+    public IEnumerable<OpenCCTestArguments> EnumOpenCCTestArguments()
+    {
+        var method = typeof(ConverterTests).GetMethod(nameof(ConverterTests.OpenCCTest));
+        var argsAttributes = method!.GetCustomAttributes(typeof(InlineDataAttribute), true).Cast<InlineDataAttribute>();
+        var cases = argsAttributes.SelectMany(a => a.GetData(method))
+            .Select(a => new OpenCCTestArguments((ChineseConversionVariant)a[0], (ChineseConversionVariant)a[1], (string)a[2]))
+            .ToList();
+        return cases;
+    }
+
+    // private IEnumerable<OpenCCTestArguments>
+
+    public class OpenCCTestArguments
+    {
+        public ChineseConversionVariant FromVariant { get; }
+
+        public ChineseConversionVariant ToVariant { get; }
+
+        public string CaseSetName { get; }
+
+        public ScriptConverterBase Converter { get; }
+
+        public List<string> TestCases { get; }
+
+        public OpenCCTestArguments(ChineseConversionVariant fromVariant, ChineseConversionVariant toVariant, string caseSetName)
+        {
+            FromVariant = fromVariant;
+            ToVariant = toVariant;
+            CaseSetName = caseSetName;
+            // FIXME sync wait on async values.
+            Converter = ChineseConversionPresets.GetConverterAsync(fromVariant, toVariant).AsTask().GetAwaiter().GetResult();
+            // We only keep input for sake of profiling.
+            TestCases = OpenCCUtils.ReadTestCases(caseSetName).Select(t => t.Input).ToList();
+        }
+
+        /// <inheritdoc />
+        public override string ToString() => $"{FromVariant} -> {ToVariant} [{CaseSetName}]";
+    }
+
 }
--- OpenCCSharp.UnitTest/Benchmarks/ConverterBulkBenchmarks.cs
+++ OpenCCSharp.UnitTest/Benchmarks/ConverterBulkBenchmarks.cs
@@ -9,47 +9,45 @@ using OpenCCSharp.Presets;
 using OpenCCSharp.UnitTest.TestCases;
 using Xunit;
 
-namespace OpenCCSharp.UnitTest.Benchmarks
+namespace OpenCCSharp.UnitTest.Benchmarks;
+
+public class ConverterBulkBenchmarks
 {
 
-    public class ConverterBulkBenchmarks
+    [Benchmark]
+    [ArgumentsSource(nameof(GetBulkConversionTestArguments))]
+    public void BulkConversionTest(BulkConversionTestArguments arguments)
     {
-
-        [Benchmark]
-        [ArgumentsSource(nameof(GetBulkConversionTestArguments))]
-        public void BulkConversionTest(BulkConversionTestArguments arguments)
-        {
-            arguments.Converter.Convert(arguments.Text);
-        }
-
-        public static IEnumerable<BulkConversionTestArguments> GetBulkConversionTestArguments()
-        {
-            var s2t = ChineseConversionPresets.GetConverterAsync(ChineseConversionVariant.Hans, ChineseConversionVariant.Hant)
-                .AsTask().GetAwaiter().GetResult();
-            var t2s = ChineseConversionPresets.GetConverterAsync(ChineseConversionVariant.Hant, ChineseConversionVariant.Hans)
-                .AsTask().GetAwaiter().GetResult();
-
-            static string BuildRepeatedSequence(int iterations)
-            {
-                // Length = 27
-                const string stem = "Open Chinese Convert 開放中文轉換";
-                var sb = new StringBuilder(stem.Length * iterations);
-                for (var i = 0; i < iterations; i++) sb.Append(stem);
-                return sb.ToString();
-            }
-
-            yield return new("Zuozhuan Hans -> Hant", OpenCCUtils.LoadBenchmarkText(), s2t);
-            yield return new("DupSequence 100 Hant -> Hans", BuildRepeatedSequence(100), t2s);
-            yield return new("DupSequence 1000 Hant -> Hans", BuildRepeatedSequence(1000), t2s);
-            yield return new("DupSequence 10000 Hant -> Hans", BuildRepeatedSequence(10000), t2s);
-            yield return new("DupSequence 100000 Hant -> Hans", BuildRepeatedSequence(100000), t2s);
-        }
-
-        public record BulkConversionTestArguments(string Name, string Text, ScriptConverterBase Converter)
-        {
-            /// <inheritdoc />
-            public override string ToString() => $"{Name} ({Text.Length} chars)";
-        }
-
+        arguments.Converter.Convert(arguments.Text);
     }
+
+    public static IEnumerable<BulkConversionTestArguments> GetBulkConversionTestArguments()
+    {
+        var s2t = ChineseConversionPresets.GetConverterAsync(ChineseConversionVariant.Hans, ChineseConversionVariant.Hant)
+            .AsTask().GetAwaiter().GetResult();
+        var t2s = ChineseConversionPresets.GetConverterAsync(ChineseConversionVariant.Hant, ChineseConversionVariant.Hans)
+            .AsTask().GetAwaiter().GetResult();
+
+        static string BuildRepeatedSequence(int iterations)
+        {
+            // Length = 27
+            const string stem = "Open Chinese Convert 開放中文轉換";
+            var sb = new StringBuilder(stem.Length * iterations);
+            for (var i = 0; i < iterations; i++) sb.Append(stem);
+            return sb.ToString();
+        }
+
+        yield return new("Zuozhuan Hans -> Hant", OpenCCUtils.LoadBenchmarkText(), s2t);
+        yield return new("DupSequence 100 Hant -> Hans", BuildRepeatedSequence(100), t2s);
+        yield return new("DupSequence 1000 Hant -> Hans", BuildRepeatedSequence(1000), t2s);
+        yield return new("DupSequence 10000 Hant -> Hans", BuildRepeatedSequence(10000), t2s);
+        yield return new("DupSequence 100000 Hant -> Hans", BuildRepeatedSequence(100000), t2s);
+    }
+
+    public record BulkConversionTestArguments(string Name, string Text, ScriptConverterBase Converter)
+    {
+        /// <inheritdoc />
+        public override string ToString() => $"{Name} ({Text.Length} chars)";
+    }
+
 }
