b2d9f74165159b02d53a20cc8e2b6df89f5df4aa
forest93 <forest93@163.com>
2022-01-23 23:45:16 +0800

    Benchmarking: Add MemoryDiagnoser.

--- OpenCCSharp.Benchmarking/Program.cs
+++ OpenCCSharp.Benchmarking/Program.cs
@@ -1,7 +1,10 @@
 ﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnosers;
 using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 using OpenCCSharp.UnitTest.Benchmarks;
 
-var config = DefaultConfig.Instance.WithSummaryStyle(SummaryStyle.Default.WithMaxParameterColumnWidth(100));
+var config = DefaultConfig.Instance
+    .WithSummaryStyle(SummaryStyle.Default.WithMaxParameterColumnWidth(100))
+    .AddDiagnoser(new MemoryDiagnoser(new MemoryDiagnoserConfig()));
 BenchmarkSwitcher.FromAssembly(typeof(ConverterBenchmarks).Assembly).Run(args, config);
