2e64e07247649bb2b86220bd633fbfb6ae7072c9
forest93 <forest93@163.com>
2022-09-13 23:49:56 +0800

    A (failed) attempt to make a CUI for OpenCC#.

---
+++ OpenCCSharp.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using OpenCCSharp.ConsoleApp;
+using Terminal.Gui;
+
+Console.WriteLine("Hello, World!");
+
+Application.Init();
+Application.Run(new MainWindow());
+Application.Shutdown();
